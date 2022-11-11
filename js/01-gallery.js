cbc44040e044342e697694a4e42f9a05f5c5c071
Tetiana Lomiichuk <lomitatiana@gmail.com>
2022-11-11 12:15:10 +0100

    Add code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryList = galleryItems
+  .map((image) => {
+    const { preview, original, description } = image;
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryList);
+
+const onSelectedImage = (event) => {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1200">
+`);
+  instance.show();
+};
+
+
+gallery.addEventListener("click", onSelectedImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imagesList = document.querySelector(".gallery");
+
+const galleryImage = galleryItems
+  .map((image) => {
+    const { preview, original, description } = image;
+    return `<li><a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a></li>`;
+  })
+  .join("");
+
+imagesList.insertAdjacentHTML("beforeend", galleryImage);
+
+const onImageClick = (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+};
+
+imagesList.addEventListener("click", onImageClick);
